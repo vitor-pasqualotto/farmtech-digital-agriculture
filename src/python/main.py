62244c97f3c7b458f4e54e80c4d961479651e3ed
Vitor Pasqualotto <175966322+vitor-pasqualotto@users.noreply.github.com>
2026-02-27 14:46:31 -0300

    feat: implementa exclusao de platacao por indice

--- src/python/main.py
+++ src/python/main.py
@@ -2,10 +2,53 @@ from menu import exibir_menu
 from calculos_area import area_retangulo, area_trapezio
 from manejo_insumos import calcular_insumo
 
-def main():
-    # Vetor que armazenará todas as plantações cadastradas
-    dados = []
+# Vetor que armazenará todas as plantações cadastradas
+dados = []
 
+def listar_dados():
+    # Verifica se existe alguma plantação cadastrada
+    if not dados:
+        print("\nNenhuma platação cadastrada.")
+
+    print("\n===== PLANTAÇÕES CADASTRADAS =====")
+
+    # Percorre o vetor mostrando as plantações cadastradas
+    for i, plantacao in enumerate(dados):
+        print(f"""
+            \nÍndice: {i}
+            Cultura: {plantacao['cultura']}
+            Área: {plantacao['area']:.2f} m²
+            Insumo: {plantacao['insumo']}
+            Qunatidade necessária: {plantacao['quantidade']:.2f}
+        """)
+
+def deletar_dados():
+    # Verifica se existe alguma plantação cadastrada
+    if not dados:
+        print("\nNenhuma plantação cadastrada para excluir.")
+        return
+
+    # Lista as plantações
+    listar_dados()
+
+    try:
+        # Pede o índice que o usuário deseja excluir
+        indice = int(input("\nDigite o índice da plantação que deseja excluir: "))
+
+        # Verifica se o índice está correto
+        if 0 <= indice < len(dados):
+            # Remove o índice do vetor
+            removido = dados.pop(indice)
+            # Exibe mensagem de sucesso
+            print(f"\nPlantação '{removido['cultura']}' removida com sucesso.")
+            
+        else:
+            print("\nÍndice inválido.")
+
+    except ValueError:
+        print("\nDigite um número válido.")
+
+def main():
     # Loop para exibir o menu
     while True:
         # Função para mostrar o menu e solicitar uma opção
@@ -14,7 +57,7 @@ def main():
         # Opção de cadastrar nova plantação
         if opcao == "1":    
             # Pede para o usuário o tipo de cultura
-            cultura = input("Digite a cultura (Milho/Café)").strip().lower()
+            cultura = input("\nDigite a cultura (Milho/Café): ").strip().lower()
 
             # Milho utiliza a área de um retângulo 
             if cultura == "milho":
@@ -59,31 +102,16 @@ def main():
 
         # Opção de listar platações cadastradas
         elif opcao == "2":
-
-            # Verifica se existe alguma plantação cadastrada
-            if not dados:
-                print("\nNenhuma platação cadastrada.")
-
-            print("\n===== PLANTAÇÕES CADASTRADAS =====")
-
-            # Percorre o vetor mostrando as plantações cadastradas
-            for i, plantacao in enumerate(dados):
-                print(f"""
-                \nÍndice: {i}
-                Cultura: {plantacao['cultura']}
-                Área: {plantacao['area']:.2f} m²
-                Insumo: {plantacao['insumo']}
-                Qunatidade necessária: {plantacao['quantidade']:.2f}
-                """)
+            # Chama função para listar dados
+            listar_dados()
 
         elif opcao == "3":
             print("Atualizar dados")
+
+        # Opção para deletar dados
         elif opcao == "4":
-            print("Deletar dados")
-        elif opcao == "5":
-            print("Saindo do programa...")
-        else:
-            print("Opção inválida!")
+            # Chama função para deletar dados
+            deletar_dados()
 
 if __name__ == "__main__":
     main()
--- src/python/menu.py
+++ src/python/menu.py
@@ -1,9 +1,13 @@
+"""
+Módulo responsável pela exibição do menu
+"""
+
 def exibir_menu():
-    print("\n===== FARMTECH DIGITAL AGRICULTURE =====")
+    print("\n===== FARMTECH DIGITAL AGRICULTURE =====\n")
     print("1 - Inserir dados")
     print("2 - Listar dados")
     print("3 - Atualizar dados")
     print("4 - Deletar dados")
     print("5 - Sair")
 
-    return input("Escolha uma opção: ")
+    return input("\nEscolha uma opção: ")
